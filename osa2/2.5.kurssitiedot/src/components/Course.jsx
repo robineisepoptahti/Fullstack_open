6cb621595816f165cc761bd02f1bbe0f4c75bce0
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2024-09-24 14:43:14 +0300

    added 2.5

---
+++ osa2/2.5.kurssitiedot/src/components/Course.jsx
@@ -0,0 +1,49 @@
+const Display = ({course}) => {
+  return (
+<div> 
+  {course.parts.map(note => <p key={note.id} >{note.name} {note.exercises}</p>)}
+</div>
+  )
+}
+
+
+const Header = ({course}) => {
+    return(
+    <div> 
+      <h2>{course.name}</h2>
+    </div>
+    )
+    }
+
+  
+  
+    const Content = ({course}) => {
+      return (
+    <div> 
+      <Display course = {course} /> 
+    </div>
+      )
+    }
+  
+
+    const Total = ({course}) => {
+    var total = course.parts.reduce((sum, note) => sum + note.exercises, 0)
+    return(
+    <div> 
+      <b>Number of exercises {total}</b>
+    </div>
+    )
+    }
+
+  
+  const Course = ({course}) => {
+    return(
+    <div>
+    <Header course={course}/>
+    <Content course={course}/>
+    <Total course={course}/> 
+    </div>
+  )}
+
+
+export default Course
