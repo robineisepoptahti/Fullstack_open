7ef75fb52938ebeaf5e75dabc04b1ac3360c1962
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2024-09-25 11:35:34 +0300

    add 2.7

---
+++ osa2/2.7.puhelinluettelo/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  
+
+
+  const addName = (event) => {
+  console.log('addName')
+   event.preventDefault()
+   const if_found = persons.find(person => person.name === newName)
+   if (if_found === undefined)
+    {const nameObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')}
+  else 
+  {window.alert(`${newName} is already added to phonebook`)
+  setNewName('')}
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }  
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(entry => <div key= {entry.name}>{entry.name} <br /> <br /></div>)}
+    </div>
+  )
+
+}
+
+export default App
