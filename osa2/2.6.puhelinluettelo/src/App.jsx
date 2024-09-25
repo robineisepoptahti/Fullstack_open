0c20dd784902cf03760fdfe249e3c1671a2a14a2
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2024-09-25 10:58:21 +0300

    add 2.6

---
+++ osa2/2.6.puhelinluettelo/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  
+  const addName = (event) => {
+    console.log('addName')
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }  
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(entry => <div key= {entry.name}>{entry.name} <br /> <br /></div>)}
+    </div>
+  )
+
+}
+
+export default App
